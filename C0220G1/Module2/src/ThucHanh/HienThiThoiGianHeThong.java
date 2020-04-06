e126e8c6fbd0ac3f6fbe598728259eb18e4291fa
ngocsonqni <sonnn.qni@gmail.com>
2020-04-06 20:23:25 +0700

    add file

---
+++ C0220G1/Module2/src/ThucHanh/HienThiThoiGianHeThong.java
@@ -0,0 +1,10 @@
+package ThucHanh;
+import java.util.Date;
+import com.sun.xml.internal.ws.wsdl.writer.document.Import;
+
+public class HienThiThoiGianHeThong {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}

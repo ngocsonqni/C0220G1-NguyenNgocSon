f9ab9f214c303aebaac3e9ce06c778a5aa4cf1fd
ngocsonqni <sonnn.qni@gmail.com>
2020-04-07 00:08:48 +0700

    add file

---
+++ C0220G1/Module2/src/ThucHanh/GiaiPhuongTrinhBacNhat.java
@@ -0,0 +1,27 @@
+package ThucHanh;
+
+import java.util.Scanner;
+
+public class GiaiPhuongTrinhBacNhat {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Nhap phuong trinh bac nhat (ax+b=0): ");
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("a: ");
+            double a=scanner.nextDouble();
+            System.out.println("b: ");
+            double b=scanner.nextDouble();
+            if (a!=0){
+                double equal=-b/a;
+                System.out.println("nghiem cua phuong trinh "+a+"x + "+b+" = 0 la: "+equal);
+            }else{
+                if(b==0){
+                    System.out.println("phuong trinh co vo so nghiem");
+                } else {
+                    System.out.println("phuong trinh khong co nghiem");
+                }
+            }
+        }
+
+    }
+}
--- C0220G1/Module2/src/ThucHanh/HienThiThoiGianHeThong.java
+++ C0220G1/Module2/src/ThucHanh/HienThiThoiGianHeThong.java
@@ -1,6 +1,5 @@
 package ThucHanh;
 import java.util.Date;
-import com.sun.xml.internal.ws.wsdl.writer.document.Import;
 
 public class HienThiThoiGianHeThong {
     public static void main(String[] args) {

eee4f380e5b1cb1f0784d51875f116b9f8225592
ngocsonqni <sonnn.qni@gmail.com>
2020-04-16 08:49:06 +0700

    add file

---
+++ C0220G1/Module2/src/CauTrucDuLieuVaGiaiThuat/BaiTap/ArrayList/MyListTest.java
@@ -0,0 +1,25 @@
+package CauTrucDuLieuVaGiaiThuat.BaiTap.ArrayList;
+import java.util.ArrayList;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+
+
+        listInteger.add(0);
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        System.out.println("element 0: "+listInteger.get(0));
+        System.out.println("element 1: "+listInteger.get(1));
+        System.out.println("element 2: "+listInteger.get(2));
+        System.out.println("element 3: "+listInteger.get(3));
+        System.out.println();
+        Object[] listInteger2;
+        listInteger2=listInteger.clone();
+        for (int i=0;i<listInteger.size();i++){
+            System.out.println("element clone "+i+": "+listInteger2[i]);
+        }
+
+    }
+}
